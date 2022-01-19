1974a209de80fb214ea0d0e15608c154c6a3b061
gviejo <guillaume.viejo@gmail.com>
2022-01-19 15:42:39 -0500

    fixed bug with ttl detection

--- main.py
+++ main.py
@@ -1,40 +1,29 @@
 '''
-	Minimal working example
+    Minimal working example
 
 '''
 
 import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
 import pynapple as nap
-from matplotlib.pyplot import *
-import sys
 
-data_directory = 'your/path/to/A2929-200711'
+DATA_DIRECTORY = 'your/path/to/A2929-200711'
 
-################################################################
 # LOADING DATA
-################################################################
-data = nap.load_session(data_directory, 'neurosuite')
-
+data = nap.load_session(DATA_DIRECTORY, 'neurosuite')
 
 spikes = data.spikes
 position = data.position
 wake_ep = data.epochs['wake']
 
-################################################################
 # COMPUTING TUNING CURVES
-################################################################
 tuning_curves = nap.compute_1d_tuning_curves(spikes, position['ry'], position['ry'].time_support, 120,  minmax=(0, 2*np.pi))
 
-		
-################################################################
+
 # PLOT
-################################################################
-
-figure()
+plt.figure()
 for i in spikes:
-	subplot(6,7,i+1, projection = 'polar')
-	plot(tuning_curves[i])
-	
+    plt.subplot(6, 7, i+1, projection='polar')
+    plt.plot(tuning_curves[i])
 
-show()
+plt.show()
