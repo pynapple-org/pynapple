683ad637984c180ee46f11cfaad028f556480cbe
gviejo <guillaume.viejo@gmail.com>
2022-03-30 17:19:04 -0400

    adding tests for time series

---
+++ tests/test_interval_set.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author: gviejo
+# @Date:   2022-03-30 11:15:02
+# @Last Modified by:   gviejo
+# @Last Modified time: 2022-03-30 11:15:02
---
+++ tests/test_ts_group.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author: gviejo
+# @Date:   2022-03-30 11:14:41
+# @Last Modified by:   gviejo
+# @Last Modified time: 2022-03-30 11:14:41
