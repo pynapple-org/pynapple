27fd68a093c79ef5c07e354a35d05fa51e858ef7
gviejo <guillaume.viejo@gmail.com>
2022-05-02 17:11:43 -0400

    pushing new version

--- setup.py
+++ setup.py
@@ -53,8 +53,8 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/PeyracheLab/pynapple',
-    version='v0.2.3',
+    version='v0.2.4',
     zip_safe=False,
     long_description_content_type='text/markdown',
-    download_url='https://github.com/PeyracheLab/pynapple/archive/refs/tags/v0.2.3.tar.gz'
+    download_url='https://github.com/PeyracheLab/pynapple/archive/refs/tags/v0.2.4.tar.gz'
 )
