fix misalignment of metadata in units object in nwb

pynapple/io/interface_nwb.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: Guillaume Viejo
 # @Date:   2023-08-01 11:54:45
-# @Last Modified by:   gviejo
-# @Last Modified time: 2023-08-07 22:34:26
+# @Last Modified by:   Guillaume Viejo
+# @Last Modified time: 2023-09-11 17:55:23
 
 """
 Pynapple class to interface with NWB files.
@@ -228,11 +228,19 @@ def _make_tsgroup(obj):
 
     N = len(tsgroup)
     metainfo = {}
-    for colname, col in zip(obj.colnames, obj.columns):
-        if colname not in ["spike_times_index", "spike_times"]:
+    for coln in obj.colnames:
+        
+        if coln == "electrode_group":            
+            for e in ['location', 'x', 'y', 'z', 'imp', 'filtering', 'rel_x', 'rel_y', 'rel_z', 'reference']:
+                tmp = [eg.__getattribute__(e) for eg in obj[coln] if hasattr(eg, e)]
+                if len(tmp) == N:
+                    metainfo[e] = np.array(tmp)
+
+        if coln not in ["spike_times_index", "spike_times", "electrode_group"]:
+            col = obj[coln]
             if len(col) == N:
                 if not isinstance(col[0], (np.ndarray, list, tuple, dict, set)):
-                    metainfo[colname] = np.array(col[:])
+                    metainfo[coln] = np.array(col[:])
 
     tsgroup = nap.TsGroup(tsgroup, **metainfo)
 