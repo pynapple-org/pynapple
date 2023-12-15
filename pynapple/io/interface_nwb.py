696e99591f8d6a60651ce2c73e17cff005a77003
Luigi Petrucco <luigi.petrucco@gmail.com>
2023-12-16 00:16:17 +0100

    isorting

--- pynapple/io/interface_nwb.py
+++ pynapple/io/interface_nwb.py
@@ -19,7 +19,6 @@ from numbers import Number
 import numpy as np
 import pynwb
 from pynwb import NWBHDF5IO
-
 # from rich.console import Console
 # from rich.table import Table
 from tabulate import tabulate
