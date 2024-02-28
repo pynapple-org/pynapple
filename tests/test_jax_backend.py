0854886a7476db6ecae0f2443ed314eb5eb391bb
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2024-02-28 00:50:20 +0000

    massive speedup on convolve

--- tests/test_jax_backend.py
+++ tests/test_jax_backend.py
@@ -52,48 +52,48 @@ t2 = time()
 tsd3 = tsd_numpy_one_ep.convolve(kernel)
 print("numba", time() - t2)
 
-
-print("... testing multi epoch")
-tsd_numpy_multi_ep = NAPTYPE(t=t, d=np.asarray(d), time_support = ep)
-tsd3 = tsd_numpy_multi_ep.convolve(kernel)
-t2 = time()
-tsd3 = tsd_numpy_multi_ep.convolve(kernel)
-print("numba mutlti-ep", time() - t2)
-
-###
-# SET BACKEND JAX
-###
-nap.nap_config.set_backend("jax")
-
-tsd_jax = NAPTYPE(t=t, d=d, time_support = ep)
-
-tree_of_jax = [jnp.asarray(tsd_jax.get(s, e).d) for s, e in tsd_jax.time_support.values]
-
-
-@jax.jit
-def map_par(tree, jax_kernel):
-    return jax.tree_map(lambda x: CONV_JAX(x, jax_kernel), tree)
-
-jkernel = jnp.asarray(kernel)
-tree_of_jax = [jnp.asarray(tsd_jax.get(s, e).d) for s, e in tsd_jax.time_support.values]
-map_par(tree_of_jax, jkernel)
-
-t2 = time()
-tsd6 = map_par(tree_of_jax, kernel)
-print("jax jit pytree", time() - t2)
-
-# # from numba import jit
-
-
-
-# def decoratorcasting(func):
-	
-# 	def wrapper(array):		
-# 		return func(np.asarray(array))
-
-# 	return wrapper
-
-# @decoratorcasting
-# @jit(nopython=True)
-# def test_numba(array):
-# 	return np.sum(array)
+#
+# print("... testing multi epoch")
+# tsd_numpy_multi_ep = NAPTYPE(t=t, d=np.asarray(d), time_support = ep)
+# tsd3 = tsd_numpy_multi_ep.convolve(kernel)
+# t2 = time()
+# tsd3 = tsd_numpy_multi_ep.convolve(kernel)
+# print("numba mutlti-ep", time() - t2)
+#
+# ###
+# # SET BACKEND JAX
+# ###
+# nap.nap_config.set_backend("jax")
+#
+# tsd_jax = NAPTYPE(t=t, d=d, time_support = ep)
+#
+# tree_of_jax = [jnp.asarray(tsd_jax.get(s, e).d) for s, e in tsd_jax.time_support.values]
+#
+#
+# @jax.jit
+# def map_par(tree, jax_kernel):
+#     return jax.tree_map(lambda x: CONV_JAX(x, jax_kernel), tree)
+#
+# jkernel = jnp.asarray(kernel)
+# tree_of_jax = [jnp.asarray(tsd_jax.get(s, e).d) for s, e in tsd_jax.time_support.values]
+# map_par(tree_of_jax, jkernel)
+#
+# t2 = time()
+# tsd6 = map_par(tree_of_jax, kernel)
+# print("jax jit pytree", time() - t2)
+#
+# # # from numba import jit
+#
+#
+#
+# # def decoratorcasting(func):
+#
+# # 	def wrapper(array):
+# # 		return func(np.asarray(array))
+#
+# # 	return wrapper
+#
+# # @decoratorcasting
+# # @jit(nopython=True)
+# # def test_numba(array):
+# # 	return np.sum(array)
