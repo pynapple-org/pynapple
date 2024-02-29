f7eed3d341aa6982a530550cd3aaa2ad89fde478
gviejo <guillaume.viejo@gmail.com>
2024-02-29 10:30:37 -0500

    Update

--- tests/test_jax_backend.py
+++ tests/test_jax_backend.py
@@ -5,10 +5,10 @@ import pytest
 import jax
 import jax.numpy as jnp
 from time import time
-from pynajax.jax_core import _convolve_vec, _convolve_mat
+from pynajax.jax_core import _convolve_vec, _convolve_mat, _jit_tree_convolve_1d_kernel
 
 t = np.arange(100000)
-d = jnp.asarray(np.random.randn(100000, 10))
+d = jnp.asarray(np.random.randn(100000, 100))
 
 if d.ndim >2:
     NAPTYPE = nap.TsdTensor
@@ -25,21 +25,30 @@ nap.nap_config.set_backend("jax")
 
 ep = nap.IntervalSet(start=np.arange(0, 1000, 100), end = np.arange(0, 1000, 100)+50)
 
-tsd_jax = NAPTYPE(t=t, d=d, time_support=ep)
+tsd_jax = NAPTYPE(t=t, d=d)#, time_support=ep)
 
 kernel = np.ones(10)
+jkernel = jnp.asarray(kernel)
 
-tsd2 = tsd_jax.convolve(kernel)
+tsd2 = tsd_jax.convolve(jkernel)
 t1 = time()
-tsd2 = tsd_jax.convolve(kernel)
-print("pynajax convolve multi-epoch", time() - t1)
+tsd2 = tsd_jax.convolve(jkernel)
+print("pynajax convolve", time() - t1)
+print(tsd2)
 
 
 
-CONV_JAX(tsd_jax.values, jnp.asarray(kernel))
+
+# CONV_JAX(tsd_jax.values, jnp.asarray(kernel))
+out = _convolve_vec(tsd_jax.values, jkernel)
 t4 = time()
-out = _convolve_vec(tsd_jax.values, jnp.asarray(kernel))
+out = _convolve_vec(tsd_jax.values, jkernel)
 print("convolve-vec", time() - t4)
+
+data = _jit_tree_convolve_1d_kernel(d, jkernel)
+t5 = time()
+data = _jit_tree_convolve_1d_kernel(d, jkernel)
+print("_jit_tree_convolve_1d_kernel", time() - t5)
 
 ###
 # SET BACKEND NUMBA
