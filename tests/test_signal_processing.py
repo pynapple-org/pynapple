dfe6e411e8f2fed704a770f7da339d85ac754e38
Kipp Freud <freud.km@gmail.com>
2024-07-03 16:55:25 +0100

    updated function names in test

--- tests/test_signal_processing.py
+++ tests/test_signal_processing.py
@@ -6,20 +6,20 @@ import pytest
 import pynapple as nap
 
 
-def test_compute_spectrum():
+def test_compute_spectogram():
     t = np.linspace(0, 1, 1024)
     sig = nap.Tsd(d=np.random.random(1024), t=t)
-    r = nap.compute_spectrum(sig)
+    r = nap.compute_spectogram(sig)
     assert len(r[1]) == 1024
     assert len(r[0]) == 1024
     assert r[0].dtype == np.complex128
     assert r[1].dtype == np.float64
 
 
-def test_compute_welch_spectrum():
+def test_ccompute_welch_spectogram():
     t = np.linspace(0, 1, 1024)
     sig = nap.Tsd(d=np.random.random(1024), t=t)
-    r = nap.compute_welch_spectrum(sig)
+    r = nap.compute_welch_spectogram(sig)
     assert r[0].dtype == np.float64
     assert r[1].dtype == np.float64
 
