a5b4f3bdf01b2ec5ade81b3817bb780f06512303
Guillaume Viejo <guillaume.viejo@gmail.com>
2024-08-05 15:48:04 -0400

    Updating tutorial_signal_processing notebool

--- docs/examples/tutorial_signal_processing.py
+++ docs/examples/tutorial_signal_processing.py
@@ -60,28 +60,33 @@ if path not in os.listdir("."):
 # Let's load and print the full dataset.
 
 data = nap.load_file(path)
-FS = 1250
+
 print(data)
 
+# %%
+# First we can extract the data from the NWB. The local field potential has been downsampled to 1250Hz. We will call it `eeg`. 
+#
+# The `time_support` of the object `data['position']` contains the interval for which the rat was running along the linear track. We will call it `wake_ep`.
+#
+
+FS = 1250
+
+eeg = data['eeg']
+
+wake_ep = data['position'].time_support
 
 # %%
 # ***
-# Selecting slices
+# Selecting example
 # -----------------------------------
 # We will consider a single run of the experiment - where the rodent completes a full traversal of the linear track,
 # followed by 4 seconds of post-traversal activity.
 
-# Define the run to use for this Analysis
-run_index = 7
-# Define the IntervalSet for this run and instantiate both LFP and
-# Position TsdFrame objects
-RUN_interval = nap.IntervalSet(
-    data["forward_ep"]["start"][run_index],
-    data["forward_ep"]["end"][run_index] + 4.0,
-)
-RUN_Tsd = data["eeg"].restrict(RUN_interval)
-RUN_pos = data["position"].restrict(RUN_interval)
-print(RUN_Tsd)
+forward_ep = data['forward_ep']
+RUN_interval = nap.IntervalSet(forward_ep.start[7], forward_ep.end[7] + 4.0)
+
+eeg_example = eeg.restrict(RUN_interval)[:,0]
+pos_example = data['position'].restrict(RUN_interval)
 
 # %%
 # ***
@@ -93,71 +98,47 @@ axd = fig.subplot_mosaic(
     [["ephys"], ["pos"]],
     height_ratios=[1, 0.4],
 )
-
-axd["ephys"].plot(
-    RUN_Tsd[:, 0].restrict(
-        nap.IntervalSet(
-            data["forward_ep"]["start"][run_index], data["forward_ep"]["end"][run_index]
-        )
-    ),
-    label="Traversal LFP Data",
-    color="green",
-)
-axd["ephys"].plot(
-    RUN_Tsd[:, 0].restrict(
-        nap.IntervalSet(
-            data["forward_ep"]["end"][run_index],
-            data["forward_ep"]["end"][run_index] + 5.0,
-        )
-    ),
-    label="Post Traversal LFP Data",
-    color="blue",
-)
-axd["ephys"].set_title("Traversal & Post Traversal LFP")
+axd["ephys"].plot(eeg_example, label="CA1")
+axd["ephys"].set_title("EEG (1250 Hz)")
 axd["ephys"].set_ylabel("LFP (v)")
 axd["ephys"].set_xlabel("time (s)")
 axd["ephys"].margins(0)
 axd["ephys"].legend()
-axd["pos"].plot(RUN_pos, color="black")
+axd["pos"].plot(pos_example, color="black")
 axd["pos"].margins(0)
 axd["pos"].set_xlabel("time (s)")
 axd["pos"].set_ylabel("Linearized Position")
-axd["pos"].set_xlim(RUN_Tsd.index.min(), RUN_Tsd.index.max())
+axd["pos"].set_xlim(RUN_interval[0,0], RUN_interval[0,1])
+
 
 
 # %%
 # ***
 # Getting the LFP Spectrogram
 # -----------------------------------
-# Let's take the Fourier transform of our data to get an initial insight into the dominant frequencies.
+# Let's take the Fourier transform of our data to get an initial insight into the dominant frequencies during exploration (`wake_ep`).
 
-fft = nap.compute_power_spectral_density(RUN_Tsd, fs=int(FS))
-print(fft)
+
+power = nap.compute_power_spectral_density(eeg, fs=FS, ep=wake_ep)
+print(power)
+
+
 
 # %%
 # ***
 # The returned object is a pandas dataframe which uses frequencies as indexes and spectral power as values.
 #
-# Now let's plot it
+# Let's plot the power between 1 and 100 Hz. 
+#
+# The red area outlines the theta rhythm (6-12 Hz) which is proeminent in hippocampal LFP.
 
 fig, ax = plt.subplots(1, constrained_layout=True, figsize=(10, 4))
-ax.plot(
-    fft.index,
-    np.abs(fft.iloc[:, 0]),
-    alpha=0.5,
-    label="LFP Frequency Power",
-    c="blue",
-    linewidth=2,
-)
+ax.semilogy(np.abs(power[(power.index>1.0) & (power.index<100)]),alpha=0.5,label="LFP Frequency Power")
+ax.axvspan(6, 12, color = 'red', alpha=0.1)
 ax.set_xlabel("Freq (Hz)")
 ax.set_ylabel("Frequency Power")
 ax.set_title("LFP Fourier Decomposition")
-ax.set_xlim(1, 30)
-ax.axvline(9.36, c="orange", label="9.36Hz", alpha=0.5)
-ax.axvline(18.74, c="green", label="18.74Hz", alpha=0.5)
 ax.legend()
-# ax.set_yscale('log')
-# ax.set_xscale('log')
 
 # %%
 # ***
@@ -168,63 +149,41 @@ ax.legend()
 # Let's generate a wavelet decomposition to look more closely at the changing frequency powers over time.
 
 # We must define the frequency set that we'd like to use for our decomposition
-freqs = np.geomspace(5, 250, 25)
+freqs = np.geomspace(3, 250, 100)
 # Compute and print the wavelet transform on our LFP data
-mwt_RUN = nap.compute_wavelet_transform(RUN_Tsd[:, 0], fs=FS, freqs=freqs)
+mwt_RUN = nap.compute_wavelet_transform(eeg_example, fs=FS, freqs=freqs)
+
+
+# %%
+# `mwt_RUN` is a TsdFrame with each column being the convolution with one wavelet at a particular frequency.
+print(mwt_RUN)
 
 # %%
 # ***
 # Now let's plot it.
 
+fig = plt.figure(constrained_layout=True, figsize=(10, 6))
+gs = plt.GridSpec(3, 1, figure=fig, height_ratios=[1.0, 0.5, 0.1])
 
-# Define wavelet decomposition plotting function
-def plot_timefrequency(times, freqs, powers, x_ticks=5, ax=None, **kwargs):
-    if np.iscomplexobj(powers):
-        powers = abs(powers)
-    ax.imshow(powers, aspect="auto", **kwargs)
-    ax.invert_yaxis()
-    ax.set_xlabel("Time (s)")
-    ax.set_ylabel("Frequency (Hz)")
-    if isinstance(x_ticks, int):
-        x_tick_pos = np.linspace(0, times.size, x_ticks)
-        x_ticks = np.round(np.linspace(times[0], times[-1], x_ticks), 2)
-    else:
-        x_tick_pos = [np.argmin(np.abs(times - val)) for val in x_ticks]
-    ax.set(xticks=x_tick_pos, xticklabels=x_ticks)
-    y_ticks = [np.round(f, 2) for f in freqs]
-    y_ticks_pos = [np.argmin(np.abs(freqs - val)) for val in y_ticks]
-    ax.set(yticks=y_ticks_pos, yticklabels=y_ticks)
-    ax.grid(False)
+ax0 = plt.subplot(gs[0,0])
+pcmesh = ax0.pcolormesh(mwt_RUN.t, freqs, np.transpose(np.abs(mwt_RUN)))
+ax0.grid(False)
+ax0.set_yscale("log")
+ax0.set_title("Wavelet decomposition")
+cbar = plt.colorbar(pcmesh, ax=ax0, orientation='vertical')
+ax0.set_label("Amplitude")
 
+ax1 = plt.subplot(gs[1,0], sharex = ax0)
+ax1.plot(eeg_example)
+ax1.set_ylabel("LFP (v)")
 
-# And plot
-fig = plt.figure(constrained_layout=True, figsize=(10, 8))
-axd = fig.subplot_mosaic(
-    [
-        ["wd_run"],
-        ["lfp_run"],
-        ["pos_run"],
-    ],
-    height_ratios=[1.2, 0.2, 0.6],
-)
-plot_timefrequency(
-    RUN_Tsd.index.values[:],
-    freqs[:],
-    np.transpose(mwt_RUN[:, :].values),
-    ax=axd["wd_run"],
-)
-axd["wd_run"].set_title(f"Wavelet Decomposition")
-axd["lfp_run"].plot(RUN_Tsd)
-axd["pos_run"].plot(RUN_pos)
-axd["pos_run"].set_xlim(RUN_Tsd.index.min(), RUN_Tsd.index.max())
-axd["pos_run"].set_ylabel("Lin. Position (cm)")
-for k in ["lfp_run", "pos_run"]:
-    axd[k].margins(0)
-    if k != "pos_run":
-        axd[k].set_ylabel("LFP (v)")
-    axd[k].get_xaxis().set_visible(False)
-    for spine in ["top", "right", "bottom", "left"]:
-        axd[k].spines[spine].set_visible(False)
+ax1 = plt.subplot(gs[2,0], sharex = ax0)
+ax1.plot(pos_example)
+ax1.set_xlabel("Time (s)")
+ax1.set_ylabel("Pos.")
+
+plt.show()
+
 
 # %%
 # ***
@@ -254,14 +213,14 @@ axd = fig.subplot_mosaic(
     height_ratios=[1, 0.4],
 )
 
-axd["lfp_run"].plot(RUN_Tsd.index.values, RUN_Tsd[:, 0], alpha=0.5, label="LFP Data")
+axd["lfp_run"].plot(eeg_example, alpha=0.5, label="LFP Data")
 axd["lfp_run"].plot(
-    RUN_Tsd.index.values,
+    eeg_example.index.values,
     theta_band_reconstruction,
     label=f"{np.round(freqs[theta_freq_index], 2)}Hz oscillations",
 )
 axd["lfp_run"].plot(
-    RUN_Tsd.index.values,
+    eeg_example.index.values,
     theta_band_power_envelope,
     label=f"{np.round(freqs[theta_freq_index], 2)}Hz power envelope",
 )
@@ -269,14 +228,14 @@ axd["lfp_run"].plot(
 axd["lfp_run"].set_ylabel("LFP (v)")
 axd["lfp_run"].set_xlabel("Time (s)")
 axd["lfp_run"].set_title(f"{np.round(freqs[theta_freq_index], 2)}Hz oscillation power.")
-axd["pos_run"].plot(RUN_pos)
+axd["pos_run"].plot(pos_example)
 [axd[k].margins(0) for k in ["lfp_run", "pos_run"]]
 [
     axd["pos_run"].spines[sp].set_visible(False)
     for sp in ["top", "right", "bottom", "left"]
 ]
 axd["pos_run"].get_xaxis().set_visible(False)
-axd["pos_run"].set_xlim(RUN_Tsd.index.min(), RUN_Tsd.index.max())
+axd["pos_run"].set_xlim(eeg_example.index.min(), eeg_example.index.max())
 axd["pos_run"].set_ylabel("Lin. Position (cm)")
 axd["lfp_run"].legend()
 
@@ -306,19 +265,19 @@ axd = fig.subplot_mosaic(
     ],
     height_ratios=[1, 0.4],
 )
-axd["lfp_run"].plot(RUN_Tsd.index.values, RUN_Tsd[:, 0], label="LFP Data")
+axd["lfp_run"].plot(eeg_example, label="LFP Data")
 axd["lfp_run"].set_ylabel("LFP (v)")
 axd["lfp_run"].set_xlabel("Time (s)")
 axd["lfp_run"].margins(0)
 axd["lfp_run"].set_title(f"Traversal & Post-Traversal LFP")
-axd["rip_pow"].plot(RUN_Tsd.index.values, ripple_power)
+axd["rip_pow"].plot(eeg_example.index.values, ripple_power)
 axd["rip_pow"].margins(0)
 axd["rip_pow"].get_xaxis().set_visible(False)
 axd["rip_pow"].spines["top"].set_visible(False)
 axd["rip_pow"].spines["right"].set_visible(False)
 axd["rip_pow"].spines["bottom"].set_visible(False)
 axd["rip_pow"].spines["left"].set_visible(False)
-axd["rip_pow"].set_xlim(RUN_Tsd.index.min(), RUN_Tsd.index.max())
+axd["rip_pow"].set_xlim(eeg_example.index.min(), eeg_example.index.max())
 axd["rip_pow"].set_ylabel(f"{np.round(freqs[ripple_freq_idx], 2)}Hz Power")
 
 # %%
@@ -352,7 +311,7 @@ axd = fig.subplot_mosaic(
     ],
     height_ratios=[1, 0.4],
 )
-axd["lfp_run"].plot(RUN_Tsd[:, 0], label="LFP Data")
+axd["lfp_run"].plot(eeg_example, label="LFP Data")
 axd["rip_pow"].plot(smoother_swr_power)
 axd["rip_pow"].plot(is_ripple, color="red", linewidth=2)
 axd["lfp_run"].set_ylabel("LFP (v)")
@@ -362,7 +321,7 @@ axd["rip_pow"].axhline(threshold)
 [axd[k].margins(0) for k in ["lfp_run", "rip_pow"]]
 [axd["rip_pow"].spines[sp].set_visible(False) for sp in ["top", "left", "right"]]
 axd["rip_pow"].get_xaxis().set_visible(False)
-axd["rip_pow"].set_xlim(RUN_Tsd.index.min(), RUN_Tsd.index.max())
+axd["rip_pow"].set_xlim(eeg_example.index.min(), eeg_example.index.max())
 axd["rip_pow"].set_ylabel(f"{np.round(freqs[ripple_freq_idx], 2)}Hz Power")
 
 # %%
@@ -374,7 +333,7 @@ axd["rip_pow"].set_ylabel(f"{np.round(freqs[ripple_freq_idx], 2)}Hz Power")
 fig, ax = plt.subplots(1, constrained_layout=True, figsize=(10, 4))
 buffer = 0.1
 ax.plot(
-    RUN_Tsd.restrict(
+    eeg_example.restrict(
         nap.IntervalSet(
             start=is_ripple.index.min() - buffer, end=is_ripple.index.max() + buffer
         )
@@ -383,7 +342,7 @@ ax.plot(
     label="Non-SWR LFP",
 )
 ax.plot(
-    RUN_Tsd.restrict(
+    eeg_example.restrict(
         nap.IntervalSet(start=is_ripple.index.min(), end=is_ripple.index.max())
     ),
     color="red",
