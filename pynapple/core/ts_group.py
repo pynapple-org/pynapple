64ffdc0658b8507a814044cf643b4f4b8cce9f81
Erik Schomburg <eschomburg@flatironinstitute.org>
2024-08-09 14:37:05 -0400

    black formatting

--- pynapple/core/ts_group.py
+++ pynapple/core/ts_group.py
@@ -138,7 +138,8 @@ class TsGroup(UserDict):
 
         data = {keys[j]: data[k] for j, k in enumerate(data.keys())}
         self.index = np.sort(keys)
-        data = {k: data[k] for k in self.index}  # Make sure data dict and index are ordered the same
+        # Make sure data dict and index are ordered the same
+        data = {k: data[k] for k in self.index}
 
         self._metadata = pd.DataFrame(index=self.index, columns=["rate"], dtype="float")
 
