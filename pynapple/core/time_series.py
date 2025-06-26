f38999980172a72aa7792aea22f61a0825dc15a1
wulfdewolf <wolf2wulf@gmail.com>
2025-06-26 21:36:53 +0000

    fix formatting

--- pynapple/core/time_series.py
+++ pynapple/core/time_series.py
@@ -179,10 +179,10 @@ class _BaseTsd(_Base, NDArrayOperatorsMixin, abc.ABC):
                 )
             self.values = d
 
-        assert len(self.index) == len(self.values), (
-            "Length of values {} does not match length of index {}".format(
-                len(self.values), len(self.index)
-            )
+        assert len(self.index) == len(
+            self.values
+        ), "Length of values {} does not match length of index {}".format(
+            len(self.values), len(self.index)
         )
 
         if isinstance(time_support, IntervalSet) and len(self.index):
@@ -1337,9 +1337,9 @@ class TsdFrame(_BaseTsd, _MetadataMixin):
         if c is None or len(c) != self.values.shape[1]:
             c = np.arange(self.values.shape[1], dtype="int")
         else:
-            assert len(c) == self.values.shape[1], (
-                "Number of columns should match the second dimension of d"
-            )
+            assert (
+                len(c) == self.values.shape[1]
+            ), "Number of columns should match the second dimension of d"
 
         self.columns = pd.Index(c)
         self.nap_class = self.__class__.__name__
