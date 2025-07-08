e0fe455908a43a6939d7df2453ea2eaaa09e74c1
Wolf De Wulf <wolf.de.wulf@ed.ac.uk>
2025-07-08 22:32:53 +0000

    Update pynapple/core/time_series.py
    Co-authored-by: Sarah Jo Venditto <sjvenditto@gmail.com>

--- pynapple/core/time_series.py
+++ pynapple/core/time_series.py
@@ -1033,6 +1033,7 @@ class TsdTensor(_BaseTsd):
         slice(np.int64(1), np.int64(3), None)
 
         Slice a single value:
+        
         >>> tsdtensor.get_slice(1.2)
         slice(np.int64(1), np.int64(2), None)
         >>> tsdtensor.get_slice(2.0)
