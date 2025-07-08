d30b6d79879e58b9b216642e210fe17ac2db844c
Wolf De Wulf <wolf.de.wulf@ed.ac.uk>
2025-07-08 22:33:18 +0000

    Update pynapple/core/time_series.py
    Co-authored-by: Sarah Jo Venditto <sjvenditto@gmail.com>

--- pynapple/core/time_series.py
+++ pynapple/core/time_series.py
@@ -3394,6 +3394,7 @@ class Ts(_Base):
         >>> ts = nap.Ts(t = [0, 1, 2, 3])
 
         Slice over a range:
+        
         >>> ts.get_slice(1.2, 2.6)
         slice(np.int64(2), np.int64(3), None)
         >>> ts.get_slice(1.0, 2.0)
