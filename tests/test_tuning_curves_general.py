e873d2f3af05309b79d7a163eccb9955a674035a
wulfdewolf <wolf2wulf@gmail.com>
2025-07-08 16:31:33 +0000

    hard-coded tc tests

--- tests/test_tuning_curves_general.py
+++ tests/test_tuning_curves_general.py
@@ -12,16 +12,17 @@ import pynapple as nap
 
 def get_group(n):
     return nap.TsGroup(
-        {i: nap.Ts(t=np.arange(0, 200, 10**i)) for i in range(-1, n - 1)}
+        {i + 1: nap.Ts(t=np.arange(0, 100, 10 ** (i - 1))) for i in range(n)}
     )
 
 
 def get_features(n, fs=10.0):
     return nap.TsdFrame(
-        t=np.arange(0, 200, 1 / fs),
-        d=np.stack([np.arange(0, 200, 1 / fs) % i for i in range(1, n + 1)], axis=1),
-        time_support=nap.IntervalSet(0, 200),
-        columns=[f"f{i}" for i in range(1, n + 1)],
+        t=np.arange(0, 100, 1 / fs),
+        d=np.stack(
+            [np.arange(0, 100, 1 / fs) % 10 * i for i in range(1, n + 1)], axis=1
+        ),
+        columns=[f"f{i}" for i in range(n)],
     )
 
 
@@ -155,130 +156,104 @@ def test_compute_tuning_curves_type_errors(group, features, kwargs, expectation)
         nap.compute_tuning_curves(group, features, **kwargs)
 
 
-@pytest.mark.filterwarnings("ignore")
 @pytest.mark.parametrize(
-    "group",
+    "group, features, kwargs, expected",
     [
-        group.count(0.1) if continuous else group
-        for continuous in [False, True]
-        for n_units in range(1, 4)
-        if (group := get_group(n_units))
-    ],
-)
-@pytest.mark.parametrize(
-    "features, bins, fs",
-    [
-        (get_features(D, fs=10.0 if fs is None else fs), bins, fs)
-        for D in range(1, 4)
-        for bins in (
-            [2, 5, 10]
-            + [list(tup) for tup in itertools.product([2, 5, 10], repeat=D) if D > 1]
-        )
-        for fs in [None, 1.0, 10.0]
-    ]
-    + [
+        # single unit, single feature
         (
-            nap.Tsd(
-                t=tsdframe.times(),
-                d=tsdframe.values.flatten(),
-                time_support=tsdframe.time_support,
+            get_group(1),
+            get_features(1),
+            {},
+            xr.DataArray(
+                np.full((1, 10), 10.0),
+                dims=["unit", "f0"],
+                coords={"unit": [1], "f0": np.linspace(0, 9.9, 11)[:-1] + 0.495},
             ),
-            num_bins,
-            fs,
-        )
-        for fs in [None, 1.0, 10.0]
-        for num_bins in [2, 5, 10]
-        if (tsdframe := get_features(1, fs=10.0 if fs is None else fs))
+        ),
+        # multiple units, single feature
+        (
+            get_group(2),
+            get_features(1),
+            {},
+            xr.DataArray(
+                np.concatenate([np.full((1, 10), 10.0), np.full((1, 10), 1.0)]),
+                dims=["unit", "f0"],
+                coords={"unit": [1, 2], "f0": np.linspace(0, 9.9, 11)[:-1] + 0.495},
+            ),
+        ),
+        # multiple units, multiple features
+        (
+            get_group(2),
+            get_features(2),
+            {},
+            xr.DataArray(
+                np.stack(
+                    [
+                        np.where(np.eye(10), 10.0, np.nan),
+                        np.where(np.eye(10), 1.0, np.nan),
+                    ]
+                ),
+                dims=["unit", "f0", "f1"],
+                coords={
+                    "unit": [1, 2],
+                    "f0": np.linspace(0, 9.9, 11)[:-1] + 0.495,
+                    "f1": np.linspace(0, 19.8, 11)[:-1] + 0.99,
+                },
+            ),
+        ),
+        # single unit, single feature, specified bins
+        (
+            get_group(1),
+            get_features(1),
+            {"bins": 5},
+            xr.DataArray(
+                np.full((1, 5), 10.0),
+                dims=["unit", "f0"],
+                coords={"unit": [1], "f0": np.linspace(0, 9.9, 6)[:-1] + 0.99},
+            ),
+        ),
+        # single unit, multiple features, specified bins
+        (
+            get_group(1),
+            get_features(2),
+            {"bins": (5, 4)},
+            xr.DataArray(
+                np.array(
+                    [
+                        [
+                            [10.0, np.nan, np.nan, np.nan],
+                            [10.0, 10.0, np.nan, np.nan],
+                            [np.nan, 10.0, 10.0, np.nan],
+                            [np.nan, np.nan, 10.0, 10.0],
+                            [np.nan, np.nan, np.nan, 10.0],
+                        ]
+                    ]
+                ),
+                dims=["unit", "f0", "f1"],
+                coords={
+                    "unit": [1],
+                    "f0": np.linspace(0, 9.9, 6)[:-1] + 0.99,
+                    "f1": np.linspace(0, 19.8, 5)[:-1] + 2.475,
+                },
+            ),
+        ),
+        # single unit, single feature, specified range
+        (
+            get_group(1),
+            get_features(1),
+            {"range": [(0, 5)]},
+            xr.DataArray(
+                np.full((1, 10), 10.0),
+                dims=["unit", "f0"],
+                coords={"unit": [1], "f0": np.linspace(0, 5.0, 11)[:-1] + 0.25},
+            ),
+        ),
     ],
 )
-@pytest.mark.parametrize("range_alpha", [None, 0.0, 0.5])
-@pytest.mark.parametrize(
-    "epochs",
-    [
-        None,
-        nap.IntervalSet(0.0, 50.0),
-        nap.IntervalSet(0.0, 100.0),
-        nap.IntervalSet(0.0, 200.0),
-        nap.IntervalSet([0.0, 40.0], [10.0, 90.0]),
-    ],
-)
-def test_compute_tuning_curves(group, features, bins, range_alpha, epochs, fs):
-    if range_alpha is None:
-        range = None
-    else:
-        full_min = np.nanmin(features.values, axis=0)
-        full_max = np.nanmax(features.values, axis=0)
-        span = full_max - full_min
-        range = np.c_[full_min + range_alpha * span, full_max - range_alpha * span]
-
-    # ------------------------------------------------------------------
-    # compute actual
-    # ------------------------------------------------------------------
-    tcs = nap.compute_tuning_curves(
-        group=group,
-        features=features,
-        bins=bins,
-        range=range,
-        epochs=epochs,
-        fs=fs,
-    )
-
-    # ------------------------------------------------------------------
-    # compute expected
-    # ------------------------------------------------------------------
-    if epochs is None:
-        epochs = features.time_support
-    else:
-        features = features.restrict(epochs)
-    group = group.restrict(epochs)
-
-    if fs is None:
-        fs = 1 / np.mean(features.time_diff(epochs=epochs))
-
-    if isinstance(features, nap.Tsd):
-        features = nap.TsdFrame(
-            d=features.values,
-            t=features.times(),
-            time_support=features.time_support,
-            columns=["f0"],
-        )
-
-    occupancy, bin_edges = np.histogramdd(features, bins=bins, range=range)
-    occupancy[occupancy == 0] = np.nan
-
-    keys = group.keys() if isinstance(group, nap.TsGroup) else group.columns
-    expected_tcs = np.zeros([len(keys), *occupancy.shape])
-    if isinstance(group, nap.TsGroup):
-        for i, n in enumerate(keys):
-            count, _ = np.histogramdd(
-                group[n].value_from(features, epochs).values,
-                bins=bin_edges,
-            )
-            expected_tcs[i] = count * fs
-    else:
-        values = group.value_from(features, epochs)
-        for i, n in enumerate(keys):
-            expected_tcs[i] = np.histogramdd(
-                values,
-                weights=group.values[:, i],
-                bins=bin_edges,
-            )[0]
-    expected_tcs /= occupancy
-
-    # expected bin centres
-    expected_tc_bins = [e[:-1] + np.diff(e) / 2 for e in bin_edges]
-
-    # ------------------------------------------------------------------
-    # test
-    # ------------------------------------------------------------------
-
-    # values
+def test_compute_tuning_curves(group, features, kwargs, expected):
+    tcs = nap.compute_tuning_curves(group, features, **kwargs)
     assert isinstance(tcs, xr.DataArray)
-    np.testing.assert_allclose(tcs, expected_tcs)
-
-    # labels
-    assert "unit" in tcs.coords
-    assert np.all(tcs.coords["unit"] == keys)
-    for dim, (dim_label, bins) in enumerate(list(tcs.coords.items())[1:]):
-        assert dim_label == features.columns[dim]
-        np.testing.assert_allclose(bins, expected_tc_bins[dim])
+    for dim in expected.coords.keys():
+        assert dim in tcs.coords
+        np.testing.assert_allclose(tcs.coords[dim].values, expected.coords[dim].values)
+    np.testing.assert_allclose(tcs, expected)
