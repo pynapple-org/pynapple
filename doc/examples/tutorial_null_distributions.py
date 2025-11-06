06bf31949c441c99012343ac4e5d3e3f39bc9e3a
wulfdewolf <wolf2wulf@gmail.com>
2025-11-06 18:23:17 +0000

    tutorial

--- doc/examples/tutorial_null_distributions.py
+++ doc/examples/tutorial_null_distributions.py
@@ -2,6 +2,8 @@
 # imports
 # ====================
 from pathlib import Path
+import matplotlib.pyplot as plt
+import numpy as np
 import pynapple as nap
 import seaborn as sns
 import xarray as xr
@@ -20,8 +22,8 @@ xr.set_options(display_expand_attrs=False)
 # data loading
 # ====================
 dandiset_id, filepath = (
-    "000582",
-    "sub-10073/sub-10073_ses-17010302_behavior+ecephys.nwb",
+    "000053",
+    "sub-npI5/sub-npI5_ses-20190414_behavior+ecephys.nwb",
 )
 with DandiAPIClient() as client:
     asset = client.get_dandiset(dandiset_id, "draft").get_asset_by_path(filepath)
@@ -31,9 +33,37 @@ fs = CachingFileSystem(
     cache_storage=str(Path("~/.caches/nwb-cache").expanduser()),
 )
 io = NWBHDF5IO(file=h5py.File(fs.open(s3_url, "rb")), load_namespaces=True)
-nwb = nap.NWBFile(io.read())
-print(nwb)
+nwb = nap.NWBFile(io.read(), lazy_loading=True)
 
 # ====================
 # data visualisation
 # ====================
+
+example_epoch = nap.IntervalSet(0, 350)
+fig, (ax_position, ax_speed) = plt.subplots(2, 1, sharex=True)
+ax_position.plot(nwb["position"].restrict(example_epoch))
+ax_position.set_ylabel("position (cm)")
+ax_speed.plot(nwb["body_speed"].restrict(example_epoch))
+ax_speed.set_ylabel("speed (cm/s)")
+plt.xlabel("time (s)")
+plt.show()
+
+# ====================
+# tuning curves
+# ====================
+print(nwb["position"].min())
+print(nwb["position"].max())
+moving = nwb["body_speed"].threshold(5, method="above").time_support
+tuning_curves = nap.compute_tuning_curves(
+    data=nwb["units"],
+    features=nwb["position"],
+    epochs=moving.intersect(nwb["trials"]),
+    feature_names=["position"],
+    bins=100,
+    range=[(0, 400)],
+)
+tuning_curves.values = (
+    tuning_curves.values - np.mean(tuning_curves.values, axis=1, keepdims=True)
+) / np.std(tuning_curves.values, axis=1, keepdims=True)
+tuning_curves.plot()
+plt.show()
