8d2174ee3ee3f6039bcfd302010f4f5182e8f93c
wulfdewolf <wolf2wulf@gmail.com>
2025-11-09 11:00:53 +0000

    tutorial

--- doc/examples/tutorial_null_distributions.py
+++ doc/examples/tutorial_null_distributions.py
@@ -3,7 +3,6 @@
 # ====================
 from pathlib import Path
 import matplotlib.pyplot as plt
-import numpy as np
 import pynapple as nap
 import seaborn as sns
 import xarray as xr
@@ -23,7 +22,8 @@ xr.set_options(display_expand_attrs=False)
 # ====================
 dandiset_id, filepath = (
     "000053",
-    "sub-npI5/sub-npI5_ses-20190414_behavior+ecephys.nwb",
+    "sub-npJ1/sub-npJ1_ses-20190521_behavior+ecephys.nwb",
+    # "sub-npI5/sub-npI5_ses-20190414_behavior+ecephys.nwb",
 )
 with DandiAPIClient() as client:
     asset = client.get_dandiset(dandiset_id, "draft").get_asset_by_path(filepath)
@@ -45,25 +45,41 @@ ax_position.plot(nwb["position"].restrict(example_epoch))
 ax_position.set_ylabel("position (cm)")
 ax_speed.plot(nwb["body_speed"].restrict(example_epoch))
 ax_speed.set_ylabel("speed (cm/s)")
+
+# for trial in nwb["trials"].intersect(example_epoch):
+#    ax_position.axvspan(trial.start[0], trial.end[0], color="gray", alpha=0.2)
 plt.xlabel("time (s)")
 plt.show()
 
 # ====================
 # tuning curves
 # ====================
+moving = nwb["body_speed"].threshold(3, method="above").time_support
+epochs = moving.intersect(nwb["trials"])
 print(nwb["position"].min())
 print(nwb["position"].max())
-moving = nwb["body_speed"].threshold(5, method="above").time_support
 tuning_curves = nap.compute_tuning_curves(
-    data=nwb["units"],
-    features=nwb["position"],
-    epochs=moving.intersect(nwb["trials"]),
+    data=nwb["units"].count(0.02),
+    features=nwb["position"].clip(0, 400),
     feature_names=["position"],
-    bins=100,
-    range=[(0, 400)],
+    epochs=epochs,
+    bins=400,
 )
-tuning_curves.values = (
-    tuning_curves.values - np.mean(tuning_curves.values, axis=1, keepdims=True)
-) / np.std(tuning_curves.values, axis=1, keepdims=True)
-tuning_curves.plot()
+# tuning_curves.values = (
+#    tuning_curves.values - np.mean(tuning_curves.values, axis=1, keepdims=True)
+# ) / np.std(tuning_curves.values, axis=1, keepdims=True)
+tuning_curves[0].plot()
 plt.show()
+
+# ====================
+# mutual information
+# ====================
+mutual_information = nap.compute_mutual_information(tuning_curves)
+print(mutual_information)
+
+# ====================
+# null distribution
+# ====================
+null_distribution = [
+    nap.shift_timestamps(nwb["units"], min_shift=20.0) for _ in range(10)
+]
